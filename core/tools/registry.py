afc7d0274000df6a8250ee371b4e839fbe2b710b
Gui97p <guipassaroto07@gmail.com>
2026-06-11 16:03:21 -0300

    More tools definitions

--- core/tools/registry.py
+++ core/tools/registry.py
@@ -94,11 +94,130 @@ TOOLS = [
         "local": True,
         "type": "function",
         "function": {
-            "name": "exit",
-            "description": "Encerra a sua execução e o programa.",
+            "name": "setClock",
+            "description": "Faz uma função baseada em relógio.",
             "parameters": {
                 "type": "object",
-                "properties": {}
+                "properties": {
+                    "feature": {
+                        "type": "string",
+                        "enum": ["alarm", "cronometer", "timer"],
+                        "description": "Tipo de função do relógio"
+                    },
+                    "hours": {
+                        "type": "integer",
+                        "description": "Horas. Para alarme: hora do disparo. Para timer: duração."
+                    },
+                    "minutes": {
+                        "type": "integer", 
+                        "description": "Minutos. Para alarme: minuto do disparo. Para timer: duração."
+                    },
+                    "seconds": {
+                        "type": "integer",
+                        "description": "Segundos. Para timer: duração."
+                    }
+                },
+                "required": ["feature"]
+            }
+        },
+    },
+    {
+        "local": True,
+        "type": "function",
+        "function": {
+            "name": "pressKey",
+            "description": "Pressiona ou segura uma key do teclado do usuário",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "key": {
+                        "type": "string",
+                        "description": "Key que deve ser pressionada. Exemplo: A, B, ENTER, SPACE, MOUSE1"
+                    },
+                    "holdTime": {
+                        "type": "number",
+                        "description": "Tempo que a key deve permanecer pressionada"
+                    }
+                },
+                "required": ["key"]
+            }
+        }
+    },
+    {
+        "local": True,
+        "type": "function",
+        "function": {
+            "name": "windowMove",
+            "description": "Move uma janela no sistema do usuário com base em posição relativa.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "activeWindows": {
+                        "type": "string",
+                        "enum": [],
+                        "description": "Nome das janelas que você pode atuar."
+                    },
+                    "x": {
+                        "type": "number",
+                        "description": "Número entre 0 e 1 que representa a posição horizontal relativa da janela"
+                    },
+                    "y": {
+                        "type": "number",
+                        "description": "Número entre 0 e 1 que representa a posição vertical relativa da janela"
+                    }
+                },
+                "required": ["activeWindows", "x", "y"]
+            }
+        }
+    },
+    {
+        "local": True,
+        "type": "function",
+        "function": {
+            "name": "windowResize",
+            "description": "Muda o tamanho de uma janela no sistema do usuário com base em tamanho relativo.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "activeWindows": {
+                        "type": "string",
+                        "enum": [],
+                        "description": "Nome das janelas que você pode atuar."
+                    },
+                    "width": {
+                        "type": "number",
+                        "description": "Número entre 0 e 1 que representa o tamanho horizontal relativo da janela"
+                    },
+                    "height": {
+                        "type": "number",
+                        "description": "Número entre 0 e 1 que representa o tamanho vertical relativo da janela"
+                    }
+                },
+                "required": ["activeWindows", "width", "height"]
+            }
+        }
+    },
+    {
+        "local": True,
+        "type": "function",
+        "function": {
+            "name": "windowContext",
+            "description": "Minimiza ou Maximiza uma janela.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "activeWindows": {
+                        "type": "string",
+                        "enum": [],
+                        "description": "Nome das janelas que você pode atuar."
+                    },
+                    "feature": {
+                        "type": "string",
+                        "enum": ["fullscreen", "window_maximize", "window_minimize"],
+                        "description": "Qual função deve ser executada"
+                    }
+                },
+                "required": ["activeWindows", "feature"]
             }
         }
     }
